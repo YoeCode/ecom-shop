cbb4380f8306bc8d939b74ea95e12914a6d3dbc2
Yoel Marti <y.marti@outlook.es>
2026-02-06 18:07:15 +0100

    feat(fileNamer): add helper for naming a files uploaded

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -7,14 +7,23 @@ import {
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { fileFilter, fileNamer } from './helpers';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
   updloadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file)
       throw new BadRequestException('make sure that the file is an image');
